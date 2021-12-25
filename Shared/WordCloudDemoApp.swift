96a58d5133a6f1266e7a47153f568f04253c5bef
tomasen <tomasen@gmail.com>
2021-12-25 16:45:34 +0800

    Initial Commit

---
+++ Shared/WordCloudDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCloudDemoApp.swift
+//  Shared
+//
+//  Created by SHEN SHENG on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WordCloudDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
